26cf0b3c9a33d8bb0897596e65e7dbb9e3b894ea
swayamshreemohanty <mohantyswayamshree@gmail.com>
2025-12-09 18:37:26 +0000

    working model

--- LedBlinker/Main.cpp
+++ LedBlinker/Main.cpp
@@ -13,7 +13,7 @@
 // Instantiate a system logger that will handle Fw::Logger::logMsg calls
 Os::Log logger;
 
-const struct device *serial = DEVICE_DT_GET(DT_NODELABEL(cdc_acm_uart0));
+const struct device *serial = DEVICE_DT_GET(DT_NODELABEL(usart3));
 
 int main()
 {
