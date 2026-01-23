fcd64e05360b48b654a6fd406f18384ad187e8a9
swayamshreemohanty <mohantyswayamshree@gmail.com>
2026-01-23 16:36:39 +0530

    removed unused files

--- project.cmake
+++ project.cmake
@@ -4,4 +4,3 @@
 add_fprime_subdirectory("${CMAKE_CURRENT_LIST_DIR}/Stm32LedBlinker/config")
 add_fprime_subdirectory("${CMAKE_CURRENT_LIST_DIR}/Components")
 add_fprime_subdirectory("${CMAKE_CURRENT_LIST_DIR}/Stm32LedBlinker/")
-# add_fprime_subdirectory("${CMAKE_CURRENT_LIST_DIR}/TestZephyr/")
